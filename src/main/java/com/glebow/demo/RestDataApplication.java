f30b2300c0f93f4d06ceabc3d2661f3fae5f3abc
Philip Glebow <philip_glebow@gap.com>
2016-07-28 10:49:44 -0700

    Spring setup

---
+++ src/main/java/com/glebow/demo/RestDataApplication.java
@@ -0,0 +1,12 @@
+package com.glebow.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDataApplication.class, args);
+	}
+}
