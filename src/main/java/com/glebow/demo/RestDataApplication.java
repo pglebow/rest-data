6fd99b301e22e098bee15ac5636b7c1120003d5b
Philip Glebow <pglebow@gmail.com>
2016-08-07 14:23:04 -0700

    Autopopulating user list

--- src/main/java/com/glebow/demo/RestDataApplication.java
+++ src/main/java/com/glebow/demo/RestDataApplication.java
@@ -6,8 +6,11 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -18,10 +21,18 @@ public class RestDataApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestDataApplication.class, args);
 	}
-	
-	@Bean(name="cacheManager")
+
+	@Bean(name = "cacheManager")
 	public CacheManager getCacheManager() {
 		ConcurrentMapCacheManager m = new ConcurrentMapCacheManager("client");
 		return m;
+	}
+
+	@Bean
+	public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
+		Resource sourceData = new ClassPathResource("users.json");
+		Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+		factory.setResources(new Resource[] { sourceData });
+		return factory;
 	}
 }
