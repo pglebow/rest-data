a3917406ba607be80043a7e1091e4b55f75acefb
Philip Glebow <pglebow@gmail.com>
2016-08-07 12:03:53 -0700

    Adding local database with Spring Data Rest / Mongo

--- src/main/java/com/glebow/demo/config/RestConfiguration.java
+++ src/main/java/com/glebow/demo/config/RestConfiguration.java
@@ -3,10 +3,13 @@
  */
 package com.glebow.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.glebow.demo.domain.User;
 
 /**
@@ -20,5 +23,17 @@ public class RestConfiguration extends RepositoryRestMvcConfiguration {
 	protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		config.exposeIdsFor(User.class);
 	}
+	
+    /**
+     * Added due to a bug in Spring Boot 1.4
+     * 
+     * {@link} https://github.com/spring-projects/spring-boot/issues/6529 
+     */
+	@Override
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return super.objectMapper();
+    }
 
 }
--- src/main/java/com/glebow/demo/repository/UserRepository.java
+++ src/main/java/com/glebow/demo/repository/UserRepository.java
@@ -6,6 +6,7 @@ package com.glebow.demo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.glebow.demo.domain.User;
@@ -17,6 +18,6 @@ import com.glebow.demo.domain.User;
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends MongoRepository<User, String> {
 
-	User findByEmail(String email);
-	List<User> findByLastName(String lastName);
+	User findByEmail(@Param("email") String email);
+	List<User> findByLastName(@Param("lastName") String lastName);
 }
