e4865cf7f47aa78dd6b2b27764e9c4bbf213d9a6
Philip Glebow <pglebow@gmail.com>
2016-12-13 10:47:30 -0800

    OAUTH2

---
+++ src/main/java/com/glebow/demo/ClientResources.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.glebow.demo;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+/**
+ * @author pglebow
+ *
+ */
+public class ClientResources {
+
+    @NestedConfigurationProperty
+    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+    @NestedConfigurationProperty
+    private ResourceServerProperties resource = new ResourceServerProperties();
+
+    public AuthorizationCodeResourceDetails getClient() {
+        return client;
+    }
+
+    public ResourceServerProperties getResource() {
+        return resource;
+    }
+}
