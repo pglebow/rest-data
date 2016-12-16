d60d64bf3e5b0e9edd79c0905e26ea3232f22e95
Philip Glebow <philip_glebow@gap.com>
2016-12-16 14:50:46 -0800

    Details and authorities

--- src/main/java/com/glebow/demo/RestDataApplication.java
+++ src/main/java/com/glebow/demo/RestDataApplication.java
@@ -18,12 +18,14 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -31,11 +33,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.CompositeFilter;
 
+import com.glebow.demo.util.Util;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableCaching
 @EnableAutoConfiguration(exclude = { EmbeddedMongoAutoConfiguration.class })
 @EnableOAuth2Client
+@EnableAuthorizationServer
 @RestController
+@EnableGlobalMethodSecurity
+@Slf4j
 public class RestDataApplication extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -52,6 +61,15 @@ public class RestDataApplication extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/").permitAll().and().csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
+
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    // http.antMatcher("/**")
+    // .authorizeRequests()
+    // .antMatchers("/", "/login**", "/webjars/**").permitAll()
+    // .anyRequest().authenticated() .and().exceptionHandling()
+    // .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"));
+    // }
 
     private Filter ssoFilter() {
         CompositeFilter filter = new CompositeFilter();
@@ -85,7 +103,11 @@ public class RestDataApplication extends WebSecurityConfigurerAdapter {
 
     @RequestMapping("/auth")
     public Principal user(Principal principal) {
-        return principal;
+        Principal retVal = principal;
+        
+        Util.logPrincipalDetails(retVal);
+        
+        return retVal;
     }
 
     @Bean
--- src/main/java/com/glebow/demo/util/Util.java
+++ src/main/java/com/glebow/demo/util/Util.java
@@ -5,12 +5,16 @@ package com.glebow.demo.util;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.Principal;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import org.json.JSONArray;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,52 +22,81 @@ import com.glebow.demo.domain.User;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author pglebow
  *
  */
+@Slf4j
 public class Util {
 
-	/**
-	 * Json Document parser
-	 * @param r
-	 * @throws IOException
-	 */
-//	public static List<Document> parseJson(URL url) throws IOException {
-//		List<Document> retVal = null;
-//		if ( url != null ) {
-//			String jsonText = Resources.toString(url, Charsets.UTF_8);
-//			JSONArray array = new JSONArray(jsonText);
-//
-//			retVal = new ArrayList<Document>(array.length());
-//			for ( int i = 0; i < array.length(); i++ ) {
-//				JSONObject o = array.getJSONObject(i);
-//				Document d = Document.parse(o.toString());
-//				retVal.add(d);
-//			}			
-//		}
-//		return Optional.ofNullable(retVal).orElse(Collections.emptyList());
-//	}
-	
-	/**
-	 * Json Document parser
-	 * @param r
-	 * @throws IOException
-	 */
-	public static Set<User> parseJsonIntoUsers(URL url) throws IOException {
-		Set<User> retVal = null;
-		if ( url != null ) {
-			String jsonText = Resources.toString(url, Charsets.UTF_8);
-			JSONArray array = new JSONArray(jsonText);
-			ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-			
-			retVal = new HashSet<User>(array.length());
-			for ( int i = 0; i < array.length(); i++ ) {
-				String s = array.getJSONObject(i).toString();
-				User user = mapper.readValue(s, User.class);
-				retVal.add(user);
-			}			
-		}
-		return Optional.ofNullable(retVal).orElse(Collections.emptySet());
-	}
+    /**
+     * Json Document parser
+     * 
+     * @param r
+     * @throws IOException
+     */
+    // public static List<Document> parseJson(URL url) throws IOException {
+    // List<Document> retVal = null;
+    // if ( url != null ) {
+    // String jsonText = Resources.toString(url, Charsets.UTF_8);
+    // JSONArray array = new JSONArray(jsonText);
+    //
+    // retVal = new ArrayList<Document>(array.length());
+    // for ( int i = 0; i < array.length(); i++ ) {
+    // JSONObject o = array.getJSONObject(i);
+    // Document d = Document.parse(o.toString());
+    // retVal.add(d);
+    // }
+    // }
+    // return Optional.ofNullable(retVal).orElse(Collections.emptyList());
+    // }
+
+    /**
+     * Json Document parser
+     * 
+     * @param r
+     * @throws IOException
+     */
+    public static Set<User> parseJsonIntoUsers(URL url) throws IOException {
+        Set<User> retVal = null;
+        if (url != null) {
+            String jsonText = Resources.toString(url, Charsets.UTF_8);
+            JSONArray array = new JSONArray(jsonText);
+            ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                    false);
+
+            retVal = new HashSet<User>(array.length());
+            for (int i = 0; i < array.length(); i++) {
+                String s = array.getJSONObject(i).toString();
+                User user = mapper.readValue(s, User.class);
+                retVal.add(user);
+            }
+        }
+        return Optional.ofNullable(retVal).orElse(Collections.emptySet());
+    }
+
+    /**
+     * Logs the details portion of a user authentication, obtained from a Principal
+     * 
+     * @param p
+     */
+    public static void logPrincipalDetails(final Principal p) {
+        if (p != null && OAuth2Authentication.class.isAssignableFrom(p.getClass())) {
+            OAuth2Authentication auth = (OAuth2Authentication) p;            
+            if (auth.getUserAuthentication() != null && UsernamePasswordAuthenticationToken.class
+                    .isAssignableFrom(auth.getUserAuthentication().getClass())) {
+                UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) auth
+                        .getUserAuthentication();
+                if (token.getDetails() != null && Map.class.isAssignableFrom(token.getDetails().getClass())) {
+                    Map<?, ?> c = (Map<?, ?>) token.getDetails();
+                    for (Map.Entry<?, ?> e : c.entrySet()) {
+                        log.info(e.getKey() + "=" + e.getValue());
+                    }
+                    log.info(System.lineSeparator());                
+                }
+            }
+        }
+    }
 }
